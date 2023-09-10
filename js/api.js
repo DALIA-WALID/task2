f50e976df0ce1caa58c483435c7971364fe8ce69
DALIA-WALID <daliawalid15@gmail.com>
2023-09-10 10:33:46 +0300

    first project

---
+++ js/api.js
@@ -0,0 +1,78 @@
+/*fetch("https://jsonplaceholder.typicode.com/users?fbclid=IwAR2m82_dIwTzfzXyGip5P7arnms5jUbifhclezKWv5cgtHXpOBHcOwVb8AY").then(alldata => {
+    let data = alldata.json();
+    
+})
+)});*/
+ 
+
+//function Car(name, email, street,address,) {
+  //  this.model = model;
+  //  this.year = year;
+//}
+ // 
+c/*onst table = document.getElementById('table')
+  const th =document.querySelector('th')
+  const td =document.querySelector('td')
+let input = data.forEach(user => {
+    if (user.value == th.name) {
+        let write = td.document.innehtml("data")
+        return (write);
+    }
+    else {
+        td = [""];
+    }
+    
+
+});*/
+/*fetch("https://jsonplaceholder.typicode.com/users?fbclid=IwAR2m82_dIwTzfzXyGip5P7arnms5jUbifhclezKWv5cgtHXpOBHcOwVb8AY").then(alldata => {
+    let data = alldata.json(); //data filtered
+    const vv = document.getElementById('table');
+    let values = Object.valuesa(data);
+    let keys = Object.keys(data);
+    let th = document.querySelector('th');
+    let td = document.querySelector('td');
+    
+    const table = document.getElementById('table').innerHTML = object.values(data);
+
+    data.forEach(user => {
+        if (keys == td.className) {
+            document.table.innerHTML(values)
+        } else {
+            document.table.innerHTML = [""];
+        }
+        return(user)
+    }
+    );
+    
+});
+
+document.write(user);*/
+
+
+
+
+
+   // then(data => {
+       // const table = document.getElementById('table')
+ /*    data.forEach () {
+            const tr = document.createElement('tr');
+            const content = `<td>${user._id}</td>
+            <td>${user.name}</td>
+            <td>${user.username}</td>
+             <td>${user.email}</td>
+            <td>${user.street}</td>
+            <td>${user.address}</td>
+            <td>${user.email}</td>`;
+          tr.innerHTML = content;
+            tableBody.appendChild(tr)
+           }});
+});
+*/
+//let fun1 = () => {
+//    console.log(('hello fron node'));
+//}
+
+
+
+const mydata = requires("./js/data")
+console.log(cala)
